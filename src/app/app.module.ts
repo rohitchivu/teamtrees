e3d0e8df1944e751fa3e1426c22aab7787f71367
Rohit <rc00008@mix.wvu.edu>
2022-02-01 17:30:05 -0500

    Created top navbar, bottom navbar, sidebar, and background with images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BottomNavbar } from 'src/app/bottomnavbar/bottomnavbar.component';
+import { SideNavbar } from 'src/app/sidebar/sidebar.component';
+import { TopNavbar } from 'src/app/topnavbar/topnavbar.component';
 
 import { AppComponent } from './app.component';
+import { Background } from './background/background.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    TopNavbar, 
+    BottomNavbar, 
+    SideNavbar, 
+    Background, 
+    CardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/background/background.component.ts
@@ -0,0 +1,12 @@
+
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "background",
+    templateUrl: "background.component.html",
+    styleUrls: ["background.component.css"]
+})
+
+export class Background{
+
+}
